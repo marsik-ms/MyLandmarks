0e61e9556f7c52d9ab8489d72040ffe4cc4d8661
marsik-ms <79077490+marsik-ms@users.noreply.github.com>
2023-05-07 20:03:07 +0300

    Initial Commit

---
+++ MyLandmarks/MyLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLandmarksApp.swift
+//  MyLandmarks
+//
+//  Created by Stanislav Perepechin on 07/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
