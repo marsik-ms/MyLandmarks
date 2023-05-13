0decbdd79e31ca4169a01454ca5ecf32753884d9
marsik-ms <79077490+marsik-ms@users.noreply.github.com>
2023-05-13 17:04:16 +0300

    review

---
+++ MyLandmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MyLandmarks
+//
+//  Created by Stanislav Perepechin on 07/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
